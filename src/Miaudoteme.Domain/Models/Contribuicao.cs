2b02175d83ec5daea8c0430c4f8c32463c40d71b
fmarcelomorais <marcelomorais.program@gmail.com>
2024-06-10 11:46:56 -0300

    Commit inicial

---
+++ src/Miaudoteme.Domain/Models/Contribuicao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miaudoteme.Domain.Models
+{
+    public class Contribuicao : Entidade
+    {
+    }
+}
---
+++ src/Miaudoteme.Domain/Models/Entidade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miaudoteme.Domain.Models
+{
+    public abstract class Entidade
+    {
+        public Guid Id { get; set; }
+        protected Entidade()
+        {
+                Id = Guid.NewGuid();
+        }
+
+    }
+}
