22ebd8632cbc2fe432cfc77033e2ca0347f88f2f
fmarcelomorais <marcelomorais.program@gmail.com>
2024-06-10 14:02:51 -0300

    restore

--- src/Miaudoteme.Domain/Models/Usuario.cs
+++ src/Miaudoteme.Domain/Models/Usuario.cs
@@ -10,13 +10,5 @@ namespace Miaudoteme.Domain.Models;
 public class Usuario : Entidade
 {
     
-    public string Nome { get; private set; }
-    public string Email { get; private set; }
-    public string Senha { get; private set; }
-    public TipoUsuario TipoUsuario { get; private set; }
 
-    public Usuario(Parameters)
-    {
-        
-    }
 }
