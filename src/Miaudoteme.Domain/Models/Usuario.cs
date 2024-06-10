e8b7a9e395d44c6fdf1808ec851d984216045618
fmarcelomorais <marcelomorais.program@gmail.com>
2024-06-10 12:21:27 -0300

    feat: Cria Enum TipoUsuario

--- src/Miaudoteme.Domain/Models/Usuario.cs
+++ src/Miaudoteme.Domain/Models/Usuario.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Miaudoteme.Domain.ValueObjects;
 
 namespace Miaudoteme.Domain.Models;
 
 public class Usuario : Entidade
 {
+    
     public string Nome { get; private set; }
     public string Email { get; private set; }
     public string Senha { get; private set; }
     public TipoUsuario TipoUsuario { get; private set; }
+
+    public Usuario(Parameters)
+    {
+        
+    }
 }
---
+++ src/Miaudoteme.Domain/ValueObjects/TipoUsuario.cs
@@ -0,0 +1,8 @@
+namespace Miaudoteme.Domain.ValueObjects
+{
+    public enum TipoUsuario
+    {
+        Tutor = 1,
+        Abrigo = 2
+    }
+}
