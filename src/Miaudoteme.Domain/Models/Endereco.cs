888954cf5b1b2dbebdf770ee080d41c9f2ba29ac
fmarcelomorais <marcelomorais.program@gmail.com>
2024-06-11 08:56:54 -0300

    feat: Implementa classe Endereco

--- src/Miaudoteme.Domain/Models/Endereco.cs
+++ src/Miaudoteme.Domain/Models/Endereco.cs
@@ -8,5 +8,21 @@ namespace Miaudoteme.Domain.Models
 {
     public class Endereco : Entidade
     {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cidade {  get; private set; }
+        public string Estado { get; private set; }
+
+        public Endereco(string logradouro, string numero, string complemento, string bairro, string cidade, string estado)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+        }
     }
 }
