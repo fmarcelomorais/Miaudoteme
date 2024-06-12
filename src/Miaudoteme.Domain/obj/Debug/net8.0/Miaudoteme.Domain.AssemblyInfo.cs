f790b91b005a8b07f9c083dbb51ba54be4adec8f
fmarcelomorais <marcelomorais.program@gmail.com>
2024-06-12 10:53:59 -0300

    feat:

--- src/Miaudoteme.Domain/obj/Debug/net8.0/Miaudoteme.Domain.AssemblyInfo.cs
+++ src/Miaudoteme.Domain/obj/Debug/net8.0/Miaudoteme.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miaudoteme.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ca7488d7771cc60e95e44dc6005ca2d1b34ca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+888954cf5b1b2dbebdf770ee080d41c9f2ba29ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miaudoteme.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miaudoteme.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
